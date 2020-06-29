778e50f947942493002e2fd64c8568588b93a3fe
mika <1506612+elmika@users.noreply.github.com>
2020-06-29 09:04:47 +0200

    Added some test for Line1D intersects method

--- test/Line1DTest.php
+++ test/Line1DTest.php
@@ -11,7 +11,34 @@ class Line1DTest extends \PHPUnit\Framework\TestCase
         $this->assertTrue($line->getSize() == 1, "Size of Line is: 1");
     }
 
-    // Size 0?
+    /** @test */
+    public function it_should_calculate_its_own_size_when_0()
+    {
+        $line = new Line1D(1,1);
+        $this->assertTrue($line->getSize() == 0, "Size of Line is: 0");
+    }
 
-    // Size -1?
+    /** @test */
+    public function it_should_calculate_its_own_size_when_negative()
+    {
+        $line = new Line1D(2,1);
+        $this->assertTrue($line->getSize() == 1, "Size of Line is: 1");
+    }
+
+    /** @test */
+    public function it_should_say_when_there_is_an_intersection_with_another_line()
+    {
+        $line = new Line1D(1,4);
+        $otherLine = new Line1D(3,7);
+        $this->assertTrue($line->intersects($otherLine), "Two lines intersect partially");
+    }
+
+    /** @test */
+    public function it_should_say_when_there_is_an_intersection_with_another_bigger_line()
+    {
+        $line = new Line1D(1,4);
+        $otherLine = new Line1D(0,7);
+        $this->assertTrue($line->intersects($otherLine), "One line in within the other line");
+        $this->assertTrue($otherLine->intersects($line), "One line in within the other line");
+    }
 }
