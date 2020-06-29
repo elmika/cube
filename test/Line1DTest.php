c0d7a98705b4a301f226660d0a0a5f7876509dcf
mika <1506612+elmika@users.noreply.github.com>
2020-06-29 10:12:27 +0200

    Added test about size and negative parameter in Line1D.

--- test/Line1DTest.php
+++ test/Line1DTest.php
@@ -19,6 +19,13 @@ class Line1DTest extends \PHPUnit\Framework\TestCase
     }
 
     /** @test */
+    public function it_should_calculate_its_own_bigger_size()
+    {
+        $line = new Line1D(-1,5);
+        $this->assertTrue($line->getSize() == 6, "Size of Line is: 6");
+    }
+
+    /** @test */
     public function it_should_calculate_its_own_size_when_0()
     {
         $line = new Line1D(1,1);
